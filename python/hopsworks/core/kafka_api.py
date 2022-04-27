d178b01598c3e2c2b38e8aec0b72bff63d8e782c
Robin Andersson <robin.eric.andersson@gmail.com>
2022-04-27 10:42:18 +0200

    Add kafka utility functions (#22)

--- python/hopsworks/core/kafka_api.py
+++ python/hopsworks/core/kafka_api.py
@@ -24,6 +24,13 @@ import base64
 import textwrap
 from pathlib import Path
 
+try:
+    from io import BytesIO
+    from avro.io import DatumReader, BinaryDecoder
+    import avro.schema
+except BaseException:
+    pass
+
 
 class KafkaApi:
 
@@ -521,3 +528,30 @@ class KafkaApi:
              keystore password
         """
         return self._get_cert_pw()
+
+    def parse_avro_msg(self, msg: bytes, avro_schema: avro.schema.RecordSchema):
+        """
+        Parses an avro record using a specified avro schema
+
+        # Arguments
+            msg: the avro message to parse
+            avro_schema: the avro schema
+
+        # Returns:
+             The parsed/decoded message
+        """
+
+        reader = DatumReader(avro_schema)
+        message_bytes = BytesIO(msg)
+        decoder = BinaryDecoder(message_bytes)
+        return reader.read(decoder)
+
+    def convert_json_schema_to_avro(self, json_schema):
+        """Parses a JSON kafka topic schema into an avro schema
+
+        # Arguments
+            json_schema: the json schema to convert
+        # Returns
+            `avro.schema.RecordSchema`: The Avro record schema
+        """
+        return avro.schema.parse(json_schema)
