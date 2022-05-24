99880f20dabd7ea6dd61f8e336b76d70fb3724dc
robzor92 <robin.eric.andersson@gmail.com>
2022-05-24 14:36:41 +0200

    fix

--- python/hopsworks/connection.py
+++ python/hopsworks/connection.py
@@ -109,7 +109,7 @@ class Connection:
     @connected
     def get_secrets_api(self):
         """Get the secrets api.
-        
+
         # Returns
             `SecretsApi`: The Secrets Api handle
         """
