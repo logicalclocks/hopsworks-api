a1698da8a0d9a974cbcbf205f89d3b5c71132cd7
robzor92 <robin.eric.andersson@gmail.com>
2022-06-09 14:49:42 +0200

    fix

--- python/hopsworks/git_repo.py
+++ python/hopsworks/git_repo.py
@@ -277,7 +277,7 @@ class GitRepo:
         return f"GitRepo({self._name!r}, {self._creator.email!r}, {self._provider!r}, {self._path!r})"
 
     def get_url(self):
-        path = "/p/" + str(self._project_id) + "settings/git"
+        path = "/p/" + str(self._project_id) + "/settings/git"
         return (
             "Git clone operation finished, explore it at "
             + util.get_hostname_replaced_url(path)
