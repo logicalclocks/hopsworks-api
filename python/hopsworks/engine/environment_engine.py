20caaccaa15b651d0369d3c723c7411dcc454122
robzor92 <robin.eric.andersson@gmail.com>
2022-12-19 17:23:16 +0100

    fix

--- python/hopsworks/engine/environment_engine.py
+++ python/hopsworks/engine/environment_engine.py
@@ -46,7 +46,7 @@ class EnvironmentEngine:
 
     def _is_final_status(self, command):
         if command.status == "FAILED":
-            raise RestAPIError(
+            raise EnvironmentException(
                 "Command failed with stacktrace: \n{}".format(command.error_message)
             )
         elif command.status == "SUCCESS":
