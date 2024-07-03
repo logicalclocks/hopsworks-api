4a683b014b5f5ac686effb4fc3ff531127c30768
Aleksey Veresov <aleksey@veresov.pro>
2024-07-03 10:00:28 +0200

    Merge variable_api of hsfs

--- python/hopsworks/core/variable_api.py
+++ python/hopsworks/core/variable_api.py
@@ -1,5 +1,5 @@
 #
-#   Copyright 2022 Logical Clocks AB
+#   Copyright 2022 Hopsworks AB
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,8 +13,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+from __future__ import annotations
+
+import re
 
 from hopsworks import client
+from hopsworks.client.exceptions import RestAPIError
 
 
 class VariableApi:
@@ -43,3 +47,45 @@ class VariableApi:
             if entry["software"] == software:
                 return entry["version"]
         return None
+
+    def parse_major_and_minor(self, backend_version):
+        version_pattern = r"(\d+)\.(\d+)"
+        matches = re.match(version_pattern, backend_version)
+
+        return matches.group(1), matches.group(2)
+
+    def get_flyingduck_enabled(self):
+        _client = client.get_instance()
+        path_params = [
+            "variables",
+            "enable_flyingduck",
+        ]
+
+        resp = _client._send_request("GET", path_params)
+        return resp["successMessage"] == "true"
+
+    def get_loadbalancer_external_domain(self):
+        _client = client.get_instance()
+        path_params = [
+            "variables",
+            "loadbalancer_external_domain",
+        ]
+
+        try:
+            resp = _client._send_request("GET", path_params)
+            return resp["successMessage"]
+        except RestAPIError:
+            return ""
+
+    def get_service_discovery_domain(self):
+        _client = client.get_instance()
+        path_params = [
+            "variables",
+            "service_discovery_domain",
+        ]
+
+        try:
+            resp = _client._send_request("GET", path_params)
+            return resp["successMessage"]
+        except RestAPIError:
+            return ""
