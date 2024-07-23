709772e6ab5b17a9832e80fc3a0e05ceef7a5736
Aleksey Veresov <aleksey@veresov.pro>
2024-07-23 17:16:57 +0200

    [FSTORE-1453][APPEND] Fix the merged client (#237)
    * fixing external client
    
    * Refactor client/hopsworks
    
    * Ruff fix client/hopsworks
    
    * Fix re-initialization of client by hsfs
    
    ---------
    
    Co-authored-by: manu-sj <manu.joseph@logicalclocks.com>

--- python/hopsworks_common/client/external.py
+++ python/hopsworks_common/client/external.py
@@ -65,14 +65,6 @@ class Client(base.Client):
         self._port = port
         self._base_url = "https://" + self._host + ":" + str(self._port)
         _logger.info("Base URL: %s", self._base_url)
-        self._project_name = project
-        if project is not None:
-            project_info = self._get_project_info(project)
-            self._project_id = str(project_info["projectId"])
-            _logger.debug("Setting Project ID: %s", self._project_id)
-        else:
-            self._project_id = None
-        _logger.debug("Project name: %s", self._project_name)
         self._region_name = region_name or self.DEFAULT_REGION
         _logger.debug("Region name: %s", self._region_name)
 
@@ -98,6 +90,19 @@ class Client(base.Client):
         self._cert_key = None
         self._cert_folder_base = cert_folder
         self._cert_folder = None
+
+        self._hsfs_post_init(project, engine, region_name)
+
+    def _hsfs_post_init(self, project, engine, region_name):
+        self._region_name = region_name or self._region_name or self.DEFAULT_REGION
+        self._project_name = project
+        if project is not None:
+            project_info = self._get_project_info(project)
+            self._project_id = str(project_info["projectId"])
+            _logger.debug("Setting Project ID: %s", self._project_id)
+        else:
+            self._project_id = None
+        _logger.debug("Project name: %s", self._project_name)
 
         if project is None:
             return
@@ -151,7 +156,7 @@ class Client(base.Client):
                 _spark_session._jsc.hadoopConfiguration().set(conf_key, conf_value)
 
     def download_certs(self, project):
-        res = self._materialize_certs(self, project)
+        res = self._materialize_certs(project)
         self._write_pem_file(res["caChain"], self._get_ca_chain_path())
         self._write_pem_file(res["clientCert"], self._get_client_cert_path())
         self._write_pem_file(res["clientKey"], self._get_client_key_path())
