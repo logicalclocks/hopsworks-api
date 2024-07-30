903e9afa970765f9a46a9fbcdef7b9c041097df5
Aleksey Veresov <aleksey@veresov.pro>
2024-07-30 14:46:51 +0200

    Fix client.base so that hsml.util.get_hostname_replaced_url works again (#249)

--- python/hopsworks_common/client/base.py
+++ python/hopsworks_common/client/base.py
@@ -291,3 +291,8 @@ class Client:
         )
         pem_str = pem_str + "-----END {}-----".format(pem_type) + "\n"
         return pem_str
+
+    def _replace_public_host(self, url):
+        """replace hostname to public hostname set in HOPSWORKS_PUBLIC_HOST"""
+        ui_url = url._replace(netloc=os.environ[self.HOPSWORKS_PUBLIC_HOST])
+        return ui_url
