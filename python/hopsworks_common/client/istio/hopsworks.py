3880d34d8983d21c07d247aad1eee397dbc9e715
Robin Andersson <robin.eric.andersson@gmail.com>
2024-09-17 14:12:54 +0200

    remove unused code

--- python/hopsworks_common/client/istio/hopsworks.py
+++ python/hopsworks_common/client/istio/hopsworks.py
@@ -54,7 +54,7 @@ class Client(istio.Client):
         self._port = port
         self._base_url = "http://" + self._host + ":" + str(self._port)
 
-        trust_store_path = self._get_trust_store_path()
+        trust_store_path = self._get_ca_chain_path()
         hostname_verification = (
             os.environ[self.REQUESTS_VERIFY]
             if self.REQUESTS_VERIFY in os.environ
@@ -88,116 +88,8 @@ class Client(istio.Client):
             hops_user = os.environ[self.HDFS_USER]
         return hops_user
 
-    def _get_trust_store_path(self):
-        """Convert truststore from jks to pem and return the location"""
-        ca_chain_path = Path(self.PEM_CA_CHAIN)
-        if not ca_chain_path.exists():
-            self._write_ca_chain(ca_chain_path)
-        return str(ca_chain_path)
-
-    def _write_ca_chain(self, ca_chain_path):
-        """
-        Converts JKS trustore file into PEM to be compatible with Python libraries
-        """
-        keystore_pw = self._cert_key
-        keystore_ca_cert = self._convert_jks_to_pem(
-            self._get_jks_key_store_path(), keystore_pw
-        )
-        truststore_ca_cert = self._convert_jks_to_pem(
-            self._get_jks_trust_store_path(), keystore_pw
-        )
-
-        with ca_chain_path.open("w") as f:
-            f.write(keystore_ca_cert + truststore_ca_cert)
-
-    def _convert_jks_to_pem(self, jks_path, keystore_pw):
-        """
-        Converts a keystore JKS that contains client private key,
-         client certificate and CA certificate that was used to
-         sign the certificate to PEM format and returns the CA certificate.
-        Args:
-        :jks_path: path to the JKS file
-        :pw: password for decrypting the JKS file
-        Returns:
-             strings: (ca_cert)
-        """
-        # load the keystore and decrypt it with password
-        ks = jks.KeyStore.load(jks_path, keystore_pw, try_decrypt_keys=True)
-        ca_certs = ""
-
-        # Convert CA Certificates into PEM format and append to string
-        for _alias, c in ks.certs.items():
-            ca_certs = ca_certs + self._bytes_to_pem_str(c.cert, "CERTIFICATE")
-        return ca_certs
-
-    def _bytes_to_pem_str(self, der_bytes, pem_type):
-        """
-        Utility function for creating PEM files
-
-        Args:
-            der_bytes: DER encoded bytes
-            pem_type: type of PEM, e.g Certificate, Private key, or RSA private key
-
-        Returns:
-            PEM String for a DER-encoded certificate or private key
-        """
-        pem_str = ""
-        pem_str = pem_str + "-----BEGIN {}-----".format(pem_type) + "\n"
-        pem_str = (
-            pem_str
-            + "\r\n".join(
-                textwrap.wrap(base64.b64encode(der_bytes).decode("ascii"), 64)
-            )
-            + "\n"
-        )
-        pem_str = pem_str + "-----END {}-----".format(pem_type) + "\n"
-        return pem_str
-
-    def _get_jks_trust_store_path(self):
-        """
-        Get truststore location
-
-        Returns:
-             truststore location
-        """
-        t_certificate = Path(self.T_CERTIFICATE)
-        if t_certificate.exists():
-            return str(t_certificate)
-        else:
-            username = os.environ[self.HADOOP_USER_NAME]
-            material_directory = Path(os.environ[self.MATERIAL_DIRECTORY])
-            return str(material_directory.joinpath(username + self.TRUSTSTORE_SUFFIX))
-
-    def _get_jks_key_store_path(self):
-        """
-        Get keystore location
-
-        Returns:
-             keystore location
-        """
-        k_certificate = Path(self.K_CERTIFICATE)
-        if k_certificate.exists():
-            return str(k_certificate)
-        else:
-            username = os.environ[self.HADOOP_USER_NAME]
-            material_directory = Path(os.environ[self.MATERIAL_DIRECTORY])
-            return str(material_directory.joinpath(username + self.KEYSTORE_SUFFIX))
-
-    def _get_cert_pw(self):
-        """
-        Get keystore password from local container
-
-        Returns:
-            Certificate password
-        """
-        pwd_path = Path(self.MATERIAL_PWD)
-        if not pwd_path.exists():
-            username = os.environ[self.HADOOP_USER_NAME]
-            material_directory = Path(os.environ[self.MATERIAL_DIRECTORY])
-            pwd_path = material_directory.joinpath(username + self.CERT_KEY_SUFFIX)
-
-        with pwd_path.open() as f:
-            return f.read()
+    def _get_ca_chain_path(self) -> str:
+        return os.path.join("/tmp", self.PEM_CA_CHAIN)
 
     def _get_serving_api_key(self):
         """Retrieve serving API key from environment variable."""
