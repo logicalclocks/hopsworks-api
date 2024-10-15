734d4bbca296bbd7bee6a618b45f42a42100e7fd
manu-sj <manu.joseph@logicalclocks.com>
2024-10-15 11:48:28 +0200

    Revert "[APPEND][FSTORE-1536] Add S3 endpoint  (#356)"
    This reverts commit 36ee2285c2ea1a3ba2c4ad3c7475db2f7670b292.

--- python/hsfs/storage_connector.py
+++ python/hsfs/storage_connector.py
@@ -374,15 +374,12 @@ class S3Connector(StorageConnector):
     def connector_options(self) -> Dict[str, Any]:
         """Return options to be passed to an external S3 connector library"""
         self.refetch()
-        options = {
+        return {
             "access_key": self.access_key,
             "secret_key": self.secret_key,
             "session_token": self.session_token,
             "region": self.region,
         }
-        if self.arguments.get("fs.s3a.endpoint"):
-            options["endpoint"] = self.arguments.get("fs.s3a.endpoint")
-        return options
 
     def read(
         self,
