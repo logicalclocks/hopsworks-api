84abef259b9ffd576ac7f9614a960c15c031d71b
Fabio Buso <dev.siroibaf@gmail.com>
2025-01-02 14:11:13 +0100

    Fix doc headers in the Hopsworks API documentation (#444)
    * Fix doc headers in the Hopsworks API documentation
    
    The headers in the main documentation are different than the headers in the Hopsworks API documentation.
    In particular:
    - Setup and Installation and Administration have been merged into: Setup and Administration
    - Python API has been renamed to Hopsworks API
    
    * Fix index in dropdown.js

--- docs/js/dropdown.js
+++ docs/js/dropdown.js
@@ -1,2 +1,2 @@
-document.getElementsByClassName("md-tabs__link")[7].style.display = "none";
-document.getElementsByClassName("md-tabs__link")[9].style.display = "none";
+document.getElementsByClassName("md-tabs__link")[6].style.display = "none";
+document.getElementsByClassName("md-tabs__link")[8].style.display = "none";
