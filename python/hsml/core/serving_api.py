b6c22106780b703c14b75f99f1da489e46b07cc2
Javier de la Rúa Martínez <javier@logicalclocks.com>
2025-01-09 09:51:21 +0100

    [HWORKS-1885] Add vllm-openai deployment and support for config files

--- python/hsml/core/serving_api.py
+++ python/hsml/core/serving_api.py
@@ -419,7 +419,4 @@ class ServingApi:
         ]
 
     def _get_istio_inference_path(self, deployment_instance):
-        if deployment_instance.model_server == "VLLM":
-            return ["openai", "v1", "completions"]
-
         return ["v1", "models", deployment_instance.name + ":predict"]
