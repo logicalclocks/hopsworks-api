4eed89d1cad680036b0e80023e4c2951f20564f7
Victor Jouffrey <vicatjou@gmail.com>
2025-02-27 16:03:19 +0100

    Crazy

--- workflow_inputs.sh
+++ workflow_inputs.sh
@@ -9,7 +9,17 @@ else
   loadtest_branch="main"
 fi
 
-printenv
+
+loadtest_prs=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/logicalclocks/loadtest/pulls) > loadtest_prs.json
+
+loadtest_branch=$(jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)' loadtest_prs.json) || 'main'
+
+cat loadtest_prs.json
+cat loadtest_branch
 
 yq '.ref = "main"' -i inputs.yaml
 yq '.inputs.python_max_parallel = "6"' -i inputs.yaml
@@ -17,7 +27,7 @@ yq '.inputs.pyspark_max_parallel = "4"' -i inputs.yaml
 hopsworks_domain="10.87.41.158" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
 labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
 hopsworks_api_branch=${ghprbSourceBranch} yq '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
-loadtest_branch=$loadtest_branch yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
+loadtest_branch=${loadtest_branch} yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
 short_sha=$(git rev-parse --short HEAD) yq '.inputs.short_sha = strenv(short_sha)' -i inputs.yaml
 
 yq -o=json inputs.yaml > inputs.json
