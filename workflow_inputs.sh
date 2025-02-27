5d7a2f4cb9b0cf96d2fc1d7cc93fcf4fe8eb0d00
Victor Jouffrey <vicatjou@gmail.com>
2025-02-27 14:25:07 +0100

    Change the host

--- workflow_inputs.sh
+++ workflow_inputs.sh
@@ -3,7 +3,7 @@ touch 'inputs.yaml'
 yq '.ref = "main"' -i inputs.yaml
 yq '.inputs.python_max_parallel = "6"' -i inputs.yaml
 yq '.inputs.pyspark_max_parallel = "4"' -i inputs.yaml
-hopsworks_domain="10.87.41.128" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
+hopsworks_domain="10.87.41.158" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
 labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
 hopsworks_api_branch="main" yq  '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
 loadtest_branch="main" yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
