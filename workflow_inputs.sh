a72b57f63a0a3e61d57da75db2d87bba99124038
Victor Jouffrey <vicatjou@gmail.com>
2025-02-27 13:33:14 +0100

    Fix inputs

--- workflow_inputs.sh
+++ workflow_inputs.sh
@@ -4,9 +4,10 @@ yq '.ref = "main"' -i inputs.yaml
 yq '.inputs.python_max_parallel = 6' -i inputs.yaml
 yq '.inputs.pyspark_max_parallel = 4' -i inputs.yaml
 hopsworks_domain="10.87.41.128" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
-labels="['e2e_small']" yq  '.inputs.labels = $labels' -i inputs.yaml
-hopsworks_api_branch="main" yq  '.inputs.hopsworks_api_branch = $hopsworks_api_branch' -i inputs.yaml
-loadtest_branch="main" yq '.inputs.loadtest_branch = $loadtest_branch' -i inputs.yaml
+labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
+hopsworks_api_branch="main" yq  '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
+loadtest_branch="main" yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
+short_sha=$(git rev-parse --short HEAD) yq '.inputs.short_sha = strenv(short_sha)' -i inputs.yaml
 
 yq -o=json inputs.yaml > inputs.json
 cat inputs.json
