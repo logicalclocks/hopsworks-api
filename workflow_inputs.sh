a3f27f8f93718e414b9ce68afb7a2033fc48c472
Victor Jouffrey <vicatjou@gmail.com>
2025-02-27 15:20:30 +0100

    Automatic branch

--- workflow_inputs.sh
+++ workflow_inputs.sh
@@ -5,7 +5,7 @@ yq '.inputs.python_max_parallel = "6"' -i inputs.yaml
 yq '.inputs.pyspark_max_parallel = "4"' -i inputs.yaml
 hopsworks_domain="10.87.41.158" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
 labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
-hopsworks_api_branch="main" yq  '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
+hopsworks_api_branch=$(git rev-parse --abbrev-ref HEAD) yq '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
 loadtest_branch="main" yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
 short_sha=$(git rev-parse --short HEAD) yq '.inputs.short_sha = strenv(short_sha)' -i inputs.yaml
 
