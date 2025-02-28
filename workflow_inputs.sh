a12c4007caabd2bbf69d6da0bac6447f21368cc0
Victor Jouffrey <vicatjou@gmail.com>
2025-02-28 13:58:23 +0100

    Redirect towards correct backend

--- workflow_inputs.sh
+++ workflow_inputs.sh
@@ -17,14 +17,18 @@ loadtest_prs=$(curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/logicalclocks/loadtest/pulls) > loadtest_prs.json
 
-loadtest_branch=$(jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)' loadtest_prs.json) || 'main'
+loadtest_branch=$(jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)' loadtest_prs.json)
+
+if [ -z "${loadtest_branch}" ]; then
+  loadtest_branch="main"
+fi
 
 cat loadtest_prs.json
 echo "${loadtest_branch}"
 
 yq '.ref = "main"' -i inputs.yaml
 yq '.inputs.max_parallel = "8"' -i inputs.yaml
-hopsworks_domain="10.87.41.158" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
+hopsworks_domain="10.87.41.190" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
 labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
 hopsworks_api_branch=${ghprbSourceBranch} yq '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
 loadtest_branch=${loadtest_branch} yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
