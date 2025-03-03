921eab60d85ad596dc7c2f9fb7b4090797b7a779
Victor Jouffrey <vicatjou@gmail.com>
2025-03-03 13:06:25 +0100

    Check the loadtest PRs

--- .github/workflow_inputs.sh
+++ .github/workflow_inputs.sh
@@ -10,11 +10,14 @@ else
   loadtest_branch="main"
 fi
 
-loadtest_prs=$(curl -L \
+loadtest_prs=$(curl -L -G \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
+  -d "state=open" \
   https://api.github.com/repos/logicalclocks/loadtest/pulls) > loadtest_prs.json
+
+cat loadtest_prs.json
 
 loadtest_branch=$(jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)' loadtest_prs.json)
 
@@ -34,6 +37,7 @@ labels="['e2e_small']" yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
 hopsworks_api_branch=${ghprbSourceBranch} yq '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
 loadtest_branch=${loadtest_branch} yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
 short_sha=$SHORT_SHA yq '.inputs.short_sha = strenv(short_sha)' -i inputs.yaml
+# repo_api_author=${ghprbPullAuthorLogin} yq '.inputs.repo_api_author = strenv(repo_api_author)' -i inputs.yaml
 
 yq -o=json inputs.yaml > inputs.json
 cat inputs.json
