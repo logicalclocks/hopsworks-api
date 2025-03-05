04501ba5193e76ff6410c9d115e923b9306363d6
Victor Jouffrey <vicatjou@gmail.com>
2025-03-05 15:04:09 +0100

    Set automatically same PR

--- .github/workflow_inputs.sh
+++ .github/workflow_inputs.sh
@@ -16,56 +16,41 @@ if [[ ${ghprbPullTitle} =~ (FSTORE-[0-9]+) || ${ghprbPullTitle} =~ (HWORKS-[0-9]
     https://api.github.com/repos/logicalclocks/loadtest/pulls)
 
   loadtest_branch=$(echo "${loadtest_prs}" | jq -r --arg captured_string ${captured_string} '.[] | select(.title | contains($captured_string)) | .head.ref')
-  minikube_ip_labels=$(echo "${loadtest_prs}" | jq -r --arg captured_string ${captured_string} '.[] | select(.title | contains($captured_string)) | .labels[] | select(.name | contains("10.87.")) | .name') 
-  
-  echo "Found labels: ${input_labels}"
+  minikube_ip=$(echo "${loadtest_prs}" | jq -r --arg captured_string ${captured_string} '.[] | select(.title | contains($captured_string)) | .labels[] | select(.name | contains("10.87.")) | .name')
+  labels=$(echo "${loadtest_prs}" | jq -r --arg captured_string ${captured_string} '.[] | select(.title | contains($captured_string)) | .labels[] | select(.name | contains("e2e")) | .name')
+fi
 
-  if [ -z "${loadtest_branch}" ]; then
+if [ -z "${loadtest_branch}" ]; then
     echo "No corresponding pr found in loadtest repo, using main branch"
     loadtest_branch="main"
   else
     echo "Found loadtest branch: ${loadtest_branch}"
   fi
 
-  if [ -z "${minikube_ip_labels}" ]; then
+  if [ -z "${minikube_ip}" ]; then
     echo "No minikube ip labels found, using default staging cluster" 
-    minikube_ip_labels="10.87.41.126" # Make it domain name instead of ip
+    minikube_ip"10.87.41.126" # Make it domain name instead of ip
   else
-    echo "Found minikube ip labels: ${minikube_ip_labels}"
+    echo "Found minikube ip labels: ${minikube_ip}"
   fi
-else
-  loadtest_branch="main"
-fi
 
+  if [ -z "${labels}" ]; then
+    echo "No labels found, using default e2e_small" 
+    labels="e2e_small"
+  else
+    echo "Found labels: ${labels}"
+  fi
 
-# loadtest_prs=$(curl -L -G \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-#   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   -d "state=open" \
-#   https://api.github.com/repos/logicalclocks/loadtest/pulls)
-
-# echo ${loadtest_prs}
-
-# loadtest_pr=$(echo ${loadtest_prs} | jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)')
-# echo ${loadtest_pr}
-#   || jq -r --arg api_branch ${ghprbSourceBranch} '.[] | select(.head.ref == $api_branch)')
-
-# if [ -z "${loadtest_branch}" ]; then
-#   loadtest_branch="main"
-# fi
-
-# cat loadtest_prs.json
 echo "${loadtest_branch}"
 
 # .ref is the name of the branch where the workflow dispatch will be sent.
 yq '.ref = "jenkins-ci"' -i inputs.yaml
 
 yq '.inputs.max_parallel = "8"' -i inputs.yaml
-hopsworks_domain="10.87.40.126" yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
-labels='e2e_small' yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
+hopsworks_domain=$minikube_ip yq '.inputs.hopsworks_domain = strenv(hopsworks_domain)' -i inputs.yaml
+labels=$labels yq  '.inputs.labels = strenv(labels)' -i inputs.yaml
 hopsworks_api_branch=${ghprbSourceBranch} yq '.inputs.hopsworks_api_branch = strenv(hopsworks_api_branch)' -i inputs.yaml
-loadtest_branch=${loadtest_branch} yq '.inputs.loadtest_branch = strenv(loadtest_branch)' -i inputs.yaml
+loadtest_branch=${loadtest_branch} yq '.inputs.loadtest_head_ref = strenv(loadtest_branch)' -i inputs.yaml
 short_sha=$SHORT_SHA yq '.inputs.short_sha = strenv(short_sha)' -i inputs.yaml
 user_repo_api=${ghprbPullAuthorLogin} yq '.inputs.user_repo_api = strenv(user_repo_api)' -i inputs.yaml
 
