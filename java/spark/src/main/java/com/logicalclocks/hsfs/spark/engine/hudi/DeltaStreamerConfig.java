a5bc0c3c49728f0e5eef04574baa983c1bbb31c7
Gibson Chikafa <gibbschikafa@gmail.com>
2025-09-11 15:30:11 +0200

    Do hive sync before migrating to version 6

--- java/spark/src/main/java/com/logicalclocks/hsfs/spark/engine/hudi/DeltaStreamerConfig.java
+++ java/spark/src/main/java/com/logicalclocks/hsfs/spark/engine/hudi/DeltaStreamerConfig.java
@@ -25,10 +25,12 @@ import org.apache.hudi.common.model.WriteOperationType;
 import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.HoodieTableVersion;
+import org.apache.hudi.common.util.Option;
 import org.apache.hudi.config.HoodieCleanConfig;
 import org.apache.hudi.config.HoodieIndexConfig;
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.hadoop.fs.HadoopFSUtils;
+import org.apache.hudi.hive.HiveSyncTool;
 import org.apache.hudi.index.HoodieIndex;
 import org.apache.hudi.table.upgrade.SparkUpgradeDowngradeHelper;
 import org.apache.hudi.table.upgrade.UpgradeDowngrade;
@@ -38,6 +40,7 @@ import org.apache.spark.sql.SparkSession;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -144,14 +147,15 @@ public class DeltaStreamerConfig implements Serializable {
     // During Hudi upgrades we might need to bump this version. This version matches Hudi 1.0.2
     if (metaClient.getTableConfig().contains(HoodieTableConfig.VERSION)
         && metaClient.getTableConfig().getTableVersion() != HoodieTableVersion.EIGHT) {
+      doHiveSync(writeOptions, javaSparkContext, metaClient);
       migrateToVersionSix(writeOptions, metaClient, javaSparkContext);
       metaClient.getTableConfig().setValue("hoodie.table.version",
           String.valueOf(HoodieTableVersion.EIGHT.versionCode()));
-      
       new UpgradeDowngrade(metaClient, getUpdatedWriteConfig(writeOptions, metaClient),
           new HoodieSparkEngineContext(javaSparkContext),
           SparkUpgradeDowngradeHelper.getInstance())
           .run(HoodieTableVersion.EIGHT, null);
+      LOG.info("Migration to version 8 completed");
     }
   }
   
@@ -177,5 +181,18 @@ public class DeltaStreamerConfig implements Serializable {
         .withCleanConfig(HoodieCleanConfig.newBuilder()
             .withFailedWritesCleaningPolicy(HoodieFailedWritesCleaningPolicy.EAGER).build())
         .withIndexConfig(HoodieIndexConfig.newBuilder().withIndexType(HoodieIndex.IndexType.BLOOM).build()).build();
+  }
+  
+  private void doHiveSync(Map<String, String> writeOptions, JavaSparkContext javaSparkContext,
+      HoodieTableMetaClient metaClient) {
+    Properties properties = new Properties();
+    for (Map.Entry<String, String> entry : writeOptions.entrySet()) {
+      properties.setProperty(entry.getKey(), entry.getValue());
+    }
+    properties.setProperty("hoodie.allow.empty.commit", "true");
+    properties.setProperty("hoodie.datasource.meta.sync.base.path", writeOptions.get(HudiEngine.HUDI_BASE_PATH));
+    HiveSyncTool hiveSyncTool = new HiveSyncTool(properties, javaSparkContext.hadoopConfiguration(),
+        Option.of(metaClient));
+    hiveSyncTool.syncHoodieTable();
   }
 }
