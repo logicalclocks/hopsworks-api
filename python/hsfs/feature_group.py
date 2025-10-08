44d4736cee7a06234e3034d4fc57c9d96e973549
bubriks <bubriks@gmail.com>
2025-10-08 16:19:59 +0300

    move update_table_schema

--- python/hsfs/feature_group.py
+++ python/hsfs/feature_group.py
@@ -2181,32 +2181,6 @@ class FeatureGroupBase:
             severity=severity,
         )
 
-    def update_table_schema(self):
-        """
-        Run a PySpark action to reconcile the schema of this feature group with the underlying storage.
-
-        This method triggers a schema update job in Hopsworks when using python client, ensuring that
-        the feature group schema in the metadata service matches the schema in the actual storage.
-        This is useful if there was an issue after appening new features.
-
-        !!! example
-            ```python
-            # connect to the Feature Store
-            fs = ...
-
-            # get the Feature Group instance
-            fg = fs.get_or_create_feature_group(...)
-
-            # update the table schema and wait for completion
-            fg.update_table_schema(wait=True)
-            ```
-
-        # Raises
-            `hopsworks.client.exceptions.RestAPIError`: If the backend encounters an error when handling the request.
-            `hopsworks.client.exceptions.FeatureStoreException`: If the schema update job fails.
-        """
-        engine.get_instance().update_table_schema(self)
-
     @property
     def embedding_index(self) -> Optional["EmbeddingIndex"]:
         if self._embedding_index:
@@ -3754,6 +3728,32 @@ class FeatureGroup(FeatureGroupBase):
             `hopsworks.client.exceptions.RestAPIError`: If the backend encounters an error when handling the request
         """
         self._feature_group_engine.delta_vacuum(self, retention_hours)
+
+    def update_table_schema(self):
+        """
+        Run a PySpark action to reconcile the schema of this feature group with the underlying storage.
+
+        This method triggers a schema update job in Hopsworks when using python client, ensuring that
+        the feature group schema in the metadata service matches the schema in the actual storage.
+        This is useful if there was an issue after appening new features.
+
+        !!! example
+            ```python
+            # connect to the Feature Store
+            fs = ...
+
+            # get the Feature Group instance
+            fg = fs.get_or_create_feature_group(...)
+
+            # update the table schema and wait for completion
+            fg.update_table_schema(wait=True)
+            ```
+
+        # Raises
+            `hopsworks.client.exceptions.RestAPIError`: If the backend encounters an error when handling the request.
+            `hopsworks.client.exceptions.FeatureStoreException`: If the schema update job fails.
+        """
+        engine.get_instance().update_table_schema(self)
 
     def as_of(
         self,
