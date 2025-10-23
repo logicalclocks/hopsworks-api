ac02f0f1928f15a4360a9004d5294cb66a92f792
bubriks <bubriks@gmail.com>
2025-10-23 11:07:07 +0300

    make feature dict for query service

--- python/tests/core/test_arrow_flight_client.py
+++ python/tests/core/test_arrow_flight_client.py
@@ -214,7 +214,7 @@ class TestArrowFlightClient:
         # Assert
         query_object_reference = {
             "query_string": "SELECT * FROM...",
-            "features": {"test.fg_test_1": ["intt", "stringt"]},
+            "features": {"test.fg_test_1": [{'name': 'intt', 'type': 'int'}, {'name': 'stringt', 'type': 'string'}]},
             "filters": {
                 "type": "logic",
                 "logic_type": "AND",
@@ -260,7 +260,8 @@ class TestArrowFlightClient:
         }
 
         query_object["features"] = {
-            key: sorted(value) for key, value in query_object["features"].items()
+            key: sorted(value, key=lambda obj: obj["name"])
+            for key, value in query_object["features"].items()
         }
 
         assert str(query_object_reference) == str(query_object)
@@ -285,7 +286,7 @@ class TestArrowFlightClient:
         # Assert
         query_object_reference = {
             "query_string": "SELECT * FROM...",
-            "features": {"test.fg_test_1": ["intt", "stringt"]},
+            "features": {"test.fg_test_1": [{'name': 'intt', 'type': 'int'}, {'name': 'stringt', 'type': 'string'}]},
             "filters": {
                 "type": "logic",
                 "logic_type": "SINGLE",
@@ -301,7 +302,8 @@ class TestArrowFlightClient:
         }
 
         query_object["features"] = {
-            key: sorted(value) for key, value in query_object["features"].items()
+            key: sorted(value, key=lambda obj: obj["name"])
+            for key, value in query_object["features"].items()
         }
 
         assert str(query_object_reference) == str(query_object)
@@ -323,7 +325,7 @@ class TestArrowFlightClient:
         # Assert
         query_object_reference = {
             "query_string": "SELECT * FROM...",
-            "features": {"test.fg_test_1": ["intt", "stringt"]},
+            "features": {"test.fg_test_1": [{'name': 'intt', 'type': 'int'}, {'name': 'stringt', 'type': 'string'}]},
             "filters": {
                 "type": "logic",
                 "logic_type": "SINGLE",
@@ -339,7 +341,8 @@ class TestArrowFlightClient:
         }
 
         query_object["features"] = {
-            key: sorted(value) for key, value in query_object["features"].items()
+            key: sorted(value, key=lambda obj: obj["name"])
+            for key, value in query_object["features"].items()
         }
 
         assert str(query_object_reference) == str(query_object)
@@ -361,7 +364,7 @@ class TestArrowFlightClient:
         # Assert
         query_object_reference = {
             "query_string": "SELECT * FROM...",
-            "features": {"test.fg_test_1": ["intt", "stringt"]},
+            "features": {"test.fg_test_1": [{'name': 'intt', 'type': 'int'}, {'name': 'stringt', 'type': 'string'}]},
             "filters": {
                 "type": "logic",
                 "logic_type": "SINGLE",
@@ -377,7 +380,8 @@ class TestArrowFlightClient:
         }
 
         query_object["features"] = {
-            key: sorted(value) for key, value in query_object["features"].items()
+            key: sorted(value, key=lambda obj: obj["name"])
+            for key, value in query_object["features"].items()
         }
 
         assert str(query_object_reference) == str(query_object)
@@ -411,14 +415,14 @@ class TestArrowFlightClient:
             "query_string": "SELECT * FROM...",
             "features": {
                 "test.tpch1snowflake_1": [
-                    "c_acctbal",
-                    "c_address",
-                    "c_comment",
-                    "c_custkey",
-                    "c_mktsegment",
-                    "c_name",
-                    "c_nationkey",
-                    "c_phone",
+                    {'name': 'c_acctbal', 'type': 'decimal(12,2)'},
+                    {'name': 'c_address', 'type': 'string'},
+                    {'name': 'c_comment', 'type': 'string'},
+                    {'name': 'c_custkey', 'type': 'decimal(38,0)'},
+                    {'name': 'c_mktsegment', 'type': 'string'},
+                    {'name': 'c_name', 'type': 'string'},
+                    {'name': 'c_nationkey', 'type': 'decimal(38,0)'},
+                    {'name': 'c_phone', 'type': 'string'}
                 ]
             },
             "filters": {
@@ -462,7 +466,8 @@ class TestArrowFlightClient:
         }
 
         query_object["features"] = {
-            key: sorted(value) for key, value in query_object["features"].items()
+            key: sorted(value, key=lambda obj: obj["name"])
+            for key, value in query_object["features"].items()
         }
 
         assert str(query_object_reference) == str(query_object)
