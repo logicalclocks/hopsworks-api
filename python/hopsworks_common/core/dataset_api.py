3feb15e95ee44d568052c786fe22da3dd6267504
Aleksey Veresov <aleksey@veresov.pro>
2025-11-14 14:36:55 +0100

    Fix docstring format

--- python/hopsworks_common/core/dataset_api.py
+++ python/hopsworks_common/core/dataset_api.py
@@ -717,13 +717,12 @@ class DatasetApi:
     def read_content(self, path: str, dataset_type: str = "DATASET"):
         """Read the content of a file.
 
-        Parameters:
+        # Arguments
             path: The path to the file to read.
-            dataset_type:
-                The type of dataset, can be `DATASET` or `HIVEDB`; defaults to `DATASET`.
+            dataset_type: The type of dataset, can be `DATASET` or `HIVEDB`; defaults to `DATASET`.
                 `HIVEDB` type is used to read files from Apache Hive.
 
-        Returns:
+        # Returns
             An object with `content` attribute containing the file content as bytes, or `None` if the file was not found.
         """
         _client = client.get_instance()
