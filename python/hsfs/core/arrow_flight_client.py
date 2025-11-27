86ca36b9ca45e1cf99453c5abaa255cec56321e1
kennethmhc <kennethmhc@users.noreply.github.com>
2025-11-27 10:04:55 +0100

    [APPEND][FSTORE-1922] Improve no data error message (#743)
    * improve error message
    
    * move error check

--- python/hsfs/core/arrow_flight_client.py
+++ python/hsfs/core/arrow_flight_client.py
@@ -95,6 +95,13 @@ def _is_no_metadata_found_error(exception):
     )
 
 
+def _is_no_data_found_error(exception):
+    # Error message: 'No data found for featuregroup fg_read_uvbhz.fg_polars_online_true_1. Detail: Failed....
+    return isinstance(
+        exception, pyarrow._flight.FlightServerError
+    ) and "No data found" in str(exception)
+
+
 def _should_retry_healthcheck(exception):
     return isinstance(exception, pyarrow._flight.FlightUnavailableError) or isinstance(
         exception, pyarrow._flight.FlightTimedOutError
@@ -423,8 +430,10 @@ class ArrowFlightClient:
                         raise FeatureStoreException(
                             "Hopsworks Query Service is busy right now. Please try again later."
                         ) from e
-                    elif _is_no_commits_found_error(e) or _is_no_metadata_found_error(
-                        e
+                    elif (
+                        _is_no_commits_found_error(e)
+                        or _is_no_metadata_found_error(e)
+                        or _is_no_data_found_error(e)
                     ):
                         raise FeatureStoreException(str(e).split("Details:")[0]) from e
                     else:
