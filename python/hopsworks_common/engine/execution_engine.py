7501a19b1ad6d6448e82f5a2647f2cecfbecd3c2
Robin Andersson <robin.eric.andersson@gmail.com>
2026-03-25 17:25:18 +0100

    [HWORKS-2695] job.run(await_termination=True) should raise an exception if the execution fails

--- python/hopsworks_common/engine/execution_engine.py
+++ python/hopsworks_common/engine/execution_engine.py
@@ -24,6 +24,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 
 from hopsworks_apigen import also_available_as
+from hopsworks_common import constants
 from hopsworks_common.client.exceptions import JobExecutionException, RestAPIError
 from hopsworks_common.core import dataset_api, execution_api
 
@@ -52,7 +53,7 @@ class ExecutionEngine:
             Downloaded stdout and stderr log path.
 
         Raises:
-            JobExecutionException: If path is provided but does not exist.
+            hopsworks.client.exceptions.JobExecutionException: If path is provided but does not exist.
         """
         if path is not None and not os.path.exists(path):
             raise JobExecutionException(f"Path {path} does not exist")
@@ -118,6 +119,7 @@ class ExecutionEngine:
 
         Raises:
             hopsworks.client.exceptions.RestAPIError: If the backend encounters an error when handling the request.
+            hopsworks.client.exceptions.JobExecutionException: If the execution finished with a failure status.
         """
         start_time = datetime.now()
 
@@ -174,13 +176,26 @@ class ExecutionEngine:
         ):
             time.sleep(5)  # Helps for log aggregation to flush to filesystem
 
-        if is_yarn_job and not updated_execution.success:
+        if updated_execution.success is False:
+            if is_yarn_job:
+                status = updated_execution.final_status
+            else:
+                status = updated_execution.state
             self._log.error(
-                f"Execution failed with status: {updated_execution.final_status}. See the logs for more information."
+                f"Execution failed with status: {status}. See the logs for more information."
             )
-        elif not is_yarn_job and not updated_execution.success:
-            self._log.error(
-                f"Execution failed with status: {updated_execution.state}. See the logs for more information."
+            if status == "KILLED":
+                raise JobExecutionException("The Hopsworks Job was stopped")
+            if status == "FAILED":
+                raise JobExecutionException(
+                    "The Hopsworks Job failed, use the Hopsworks UI to access the job logs"
+                )
+            if status == "FRAMEWORK_FAILURE":
+                raise JobExecutionException(
+                    "The Hopsworks Job monitoring failed, could not determine the final status"
+                )
+            raise JobExecutionException(
+                f"Execution failed with status: {status}. See the logs for more information."
             )
         else:
             self._log.info("Execution finished successfully.")
